8ad0bc7fe48feb911f3ca57ffea7f4dc24067f3d
VICJR31 <73763182+VICJR31@users.noreply.github.com>
2020-11-01 21:24:04 -0800

    init

---
+++ home_prac.php
@@ -0,0 +1,348 @@
+<!--***(07/06/2020): LOOK AT TEMPLATES FOR IMPROVEMENT ON WEBSITE DESIGN***-->
+<!--***(07/13/2020): HAVE COLOR PATTERN FOR EACH BOOK SECTION, 
+    NAV BAR FOR EASY ASSESS TO SPECIFIC BOOKS AND KEEP TEXT CONSISTENT-->
+    <!DOCTYPE html>
+<html>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="stylesheets/colors.css" type="text/css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <title>Southern Victory Series, TL-191</title>
+  </head>
+  <body>
+    <!--Add picture on either side of the title 'pb-5' -->
+    <!--***FIX TITLE TO CENTER/MIDDLE***-->
+    <!-- w-100  Flag sizes for below from Bootstrap-->
+    <div class = "jumbotron jumbotron-fluid expandTerritories">
+        <div class = "container container-fluid">
+            <div class="row">
+                <div class="col-md"> 
+                    <img src="https://www.crwflags.com/fotw/images/f/fic_hfrm7.gif" class="rounded float-left w-100 p-3" alt="The Stars and Bars">
+                </div>
+                <div class="col-md mr-md-3">
+                    <h1 class="text-center">Southern Victory Series, by Harry Turtledove</h1>
+                </div>
+                <div class="col-md">
+                    <img src="https://vignette.wikia.nocookie.net/turtledove/images/3/3f/Flag_of_the_United_States_(Southern_Victory)_.png/revision/latest?cb=20181103210612" class="rounded float-right w-100 p-3" alt="The Stars and Stripes">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--LINE ENDS HERE!!!!! sticky-top bg-light-->
+    <ul class="nav navbar justify-content-center">
+        <li class="nav-item">
+          <a class="nav-link" href="book_sections/bk1_remain.html">How Few Remain</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="book_sections/bk2_front.html">American Front</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="book_sections/bk3_hell.html">Walk in Hell</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="book_sections/bk4_breakthroughs.html">Breakthroughs</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="book_sections/bk5_iron.html">Blood and Iron </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="book_sections/bk6_hold.html">The Center Cannot Hold</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="book_sections/bk7_opposition.html">The Victorious Opposition</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="book_sections/bk8_engagement.html">Return Engagement</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="book_sections/bk9_east.html">Drive to the East</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="book_sections/bk10_grapple.html">The Grapple</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="book_sections/bk11_death.html">In at the Death</a>
+        </li>
+    </ul>
+    <!--Add picture on either side of the title 'pb-5'-->
+    <hr>
+    <!--ADD a 'Dark Mode' option here!-->
+    <!--Add link Grid here!!!-->
+    <div class="pb-3">
+        <h2 class="display-4">Intro</h2>
+        <ul>
+            <li class="lead">
+                The Southern Victory series (also given the name of Timeline-191 for fans)
+                is a collection of 11 books telling the story of what if the Confederacy won the
+                American Civil War? Along with the road the rest of the world takes because of this
+                outcome, spanning from the 1860s, all the way up to the mid-1940s.
+            </li>
+            <li class="lead">
+                With an exciting and new visual imagination on the wide parts of Northern America's
+                change of path -experienced by a large and wide variety of characters in this new world-
+                Harry Turtledove knocks it out of the park in his telling of this new piece of alternate
+                history!
+            </li>
+        </ul>
+    </div>
+    <hr>
+    <!--++++++-->
+    <div class="container">
+        <h3 class="lead pb-3">Get a quick snippet of each book's premise below, but be warn!...Spoilers ahead (read books first you won't regret it!)</h3>
+        <ul>
+            <!--Book Titles below for summaries but links to their title texts will give more details to separate pages -yes-->
+            <!-- BOOK 1 -->
+            <div class="row">
+                <div class="col-md order-12">
+                    <li class="h6"><i>How Few Remain (1997)</i></li>
+                    <p class="lead">
+                        The 33rd Waffen Grenadier Division of the SS Charlemagne (1st French) (German: 33. 
+                        Waffen-Grenadier-Division der SS "Charlemagne" (französische Nr. 1)) and 
+                        Charlemagne Regiment are collective names used for units of French 
+                        volunteers in the Wehrmacht and later Waffen-SS during World War II.
+                    </p>
+                </div>
+                <div class="col-md col-sm-8 col-md-6 col-lg-4 order-1 mx-auto pb-3">
+                    <a href="book_sections/bk1_remain.html">
+                        <!--Picture of Era here!-->
+                        <img src="https://upload.wikimedia.org/wikipedia/en/7/71/HowFewRemain%281stEd%29.jpg" 
+                        class="w-75 p-3" alt="How Few Remain Book picture">
+                    </a> 
+                </div>   
+            </div> 
+            <hr>
+            <!-- BOOK 2 -->
+            <div class="row">
+                <div class="col-md order-12 order-md-1">
+                    <li class="h6"><i>The Great War: American Front (1998)</i></li>
+                    <p class="lead">
+                        The 33rd Waffen Grenadier Division of the SS Charlemagne (1st French) (German: 33. 
+                        Waffen-Grenadier-Division der SS "Charlemagne" (französische Nr. 1)) and 
+                        Charlemagne Regiment are collective names used for units of French 
+                        volunteers in the Wehrmacht and later Waffen-SS during World War II.
+                    </p>
+                </div>
+                <div class="col-md col-sm-8 col-md-6 col-lg-4 order-1 order-md-12 mx-auto pb-3">
+                    <a href="book_sections/bk2_front.html">
+                        <!--Picture of Era here!-->
+                        <img src="https://www.sfsite.com/~silverag/american.jpg" 
+                        class="w-75 p-3" alt="American Front picture">
+                    </a> 
+                </div>   
+            </div> 
+            <hr>
+
+            <!-- BOOK 3 -->
+            <div class="row">
+                <div class="col-md order-12">
+                    <li class="h6"><i>The Great War: Walk in Hell (1999)</i></li>
+                    <p class="lead">
+                        The 33rd Waffen Grenadier Division of the SS Charlemagne (1st French) (German: 33. 
+                        Waffen-Grenadier-Division der SS "Charlemagne" (französische Nr. 1)) and 
+                        Charlemagne Regiment are collective names used for units of French 
+                        volunteers in the Wehrmacht and later Waffen-SS during World War II.
+                    </p>
+                </div>
+                <div class="col-md col-sm-8 col-md-6 col-lg-4 order-1 mx-auto pb-3">
+                    <a href="book_sections/bk3_hell.html">
+                        <!--Picture of Era here!-->
+                        <img src="https://img1.od-cdn.com/ImageType-100/0111-1/%7BB139E3D0-2B43-4010-8FA9-707D829010B6%7DImg100.jpg" 
+                        class="w-75 p-3" alt="Walk in Hell Book picture">
+                    </a> 
+                </div>   
+            </div> 
+            <hr>
+
+            <!-- BOOK 4 -->
+            <div class="row">
+                <div class="col-md order-12 order-md-1">
+                    <li class="h6"><i>The Great War: Breakthroughs (2000)</i></li>
+                    <p class="lead">
+                        The 33rd Waffen Grenadier Division of the SS Charlemagne (1st French) (German: 33. 
+                        Waffen-Grenadier-Division der SS "Charlemagne" (französische Nr. 1)) and 
+                        Charlemagne Regiment are collective names used for units of French 
+                        volunteers in the Wehrmacht and later Waffen-SS during World War II.
+                    </p>
+                </div>
+                <div class="col-md col-sm-8 col-md-6 col-lg-4 order-1 order-md-12 mx-auto pb-3">
+                    <a href="book_sections/bk4_breakthroughs.html">
+                        <!--Picture of Era here!-->
+                        <img src="http://www.sfsite.com/~silverag/breakthroughs.jpg" 
+                        class="w-75 p-3" alt="Breakthroughs Book picture">
+                    </a> 
+                </div>   
+            </div> 
+            <hr>
+
+            <!-- BOOK 5 -->
+            <div class="row">
+                <div class="col-md order-12">
+                    <li class="h6"><i>American Empire: Blood and Iron (2001)</i></li>
+                    <p class="lead">
+                        The 33rd Waffen Grenadier Division of the SS Charlemagne (1st French) (German: 33. 
+                        Waffen-Grenadier-Division der SS "Charlemagne" (französische Nr. 1)) and 
+                        Charlemagne Regiment are collective names used for units of French 
+                        volunteers in the Wehrmacht and later Waffen-SS during World War II.
+                    </p>
+                </div>
+                <div class="col-md col-sm-8 col-md-6 col-lg-4 order-1 mx-auto pb-3">
+                    <a href="book_sections/bk5_iron.html">
+                        <!--Picture of Era here!-->
+                        <img src="https://www.sfsite.com/~silverag/bloodandiron.jpg" 
+                        class="w-75 p-3" alt="Blood and Iron Book picture">
+                    </a> 
+                </div>   
+            </div> 
+            <hr>
+
+            <!-- BOOK 6 -->
+            <div class="row">
+                <div class="col-md order-12 order-md-1">
+                    <li class="h6"><i>American Empire: The Center Cannot Hold (2002)</i></li>
+                    <p class="lead">
+                        The 33rd Waffen Grenadier Division of the SS Charlemagne (1st French) (German: 33. 
+                        Waffen-Grenadier-Division der SS "Charlemagne" (französische Nr. 1)) and 
+                        Charlemagne Regiment are collective names used for units of French 
+                        volunteers in the Wehrmacht and later Waffen-SS during World War II.
+                    </p>
+                </div>
+                <div class="col-md col-sm-8 col-md-6 col-lg-4 order-1 order-md-12 mx-auto pb-3">
+                    <a href="book_sections/bk6_hold.html">
+                        <!--Picture of Era here!-->
+                        <img src="http://img2.imagesbn.com/p/9780345454805_p0_v1_s260x420.JPG" 
+                        class="w-75 p-3" alt="Book cover of The Center Cannot Hold">
+                    </a> 
+                </div>   
+            </div> 
+            <hr>
+
+            <!-- BOOK 7 -->
+            <div class="row">
+                <div class="col-md order-12">
+                    <li class="h6"><i>American Empire: The Victorious Opposition (2003)</i></li>
+                    <p class="lead">
+                        The 33rd Waffen Grenadier Division of the SS Charlemagne (1st French) (German: 33. 
+                        Waffen-Grenadier-Division der SS "Charlemagne" (französische Nr. 1)) and 
+                        Charlemagne Regiment are collective names used for units of French 
+                        volunteers in the Wehrmacht and later Waffen-SS during World War II.
+                    </p>
+                </div>
+                <div class="col-md col-sm-8 col-md-6 col-lg-4 order-1 mx-auto pb-3">
+                    <a href="book_sections/bk7_opposition.html">
+                        <!--Picture of Era here!-->
+                        <img src="https://th.bing.com/th/id/OIP.RkCKy07dHE7Kxi-gUOev_wHaLD?pid=Api&rs=1" 
+                        class="w-75 p-3" alt="The Victorious Opposition picture">
+                    </a> 
+                </div>   
+            </div> 
+            <hr>
+
+            <!-- BOOK 8 -->
+            <div class="row">
+                <div class="col-md order-12 order-md-1">
+                    <li class="h6"><i>Settling Accounts: Return Engagement (2004)</i></li>
+                    <p class="lead">
+                        The 33rd Waffen Grenadier Division of the SS Charlemagne (1st French) (German: 33. 
+                        Waffen-Grenadier-Division der SS "Charlemagne" (französische Nr. 1)) and 
+                        Charlemagne Regiment are collective names used for units of French 
+                        volunteers in the Wehrmacht and later Waffen-SS during World War II.
+                    </p>
+                </div>
+                <div class="col-md col-sm-8 col-md-6 col-lg-4 order-1 order-md-12 mx-auto pb-3">
+                    <a href="book_sections/bk8_engagement.html">
+                        <!--Picture of Era here!-->
+                        <img src="http://covers.openlibrary.org/b/id/210798-M.jpg" 
+                        class="w-75 p-3" alt="Return Engagement picture">
+                    </a> 
+                </div>   
+            </div> 
+            <hr>
+    
+            <!-- BOOK 9 -->
+            <div class="row">
+                <div class="col-md order-12">
+                    <li class="h6"><i>Settling Accounts: Drive to the East (2005)</i></li>
+                    <p class="lead">
+                        The 33rd Waffen Grenadier Division of the SS Charlemagne (1st French) (German: 33. 
+                        Waffen-Grenadier-Division der SS "Charlemagne" (französische Nr. 1)) and 
+                        Charlemagne Regiment are collective names used for units of French 
+                        volunteers in the Wehrmacht and later Waffen-SS during World War II.
+                    </p>
+                </div>
+                <div class="col-md col-sm-8 col-md-6 col-lg-4 order-1 mx-auto pb-3">
+                    <a href="book_sections/bk9_east.html">
+                        <!--Picture of Era here!-->
+                        <img src="https://th.bing.com/th/id/OIP.LViewmCp0L2zyFdosngegAHaLC?pid=Api&rs=1" 
+                        class="w-75 p-3" alt="Book cover of Drive to the East">
+                    </a> 
+                </div>   
+            </div> 
+            <hr>
+
+            <!-- BOOK 10 -->
+            <div class="row">
+                <div class="col-md order-12 order-md-1">
+                    <li class="h6"><i>Settling Accounts: The Grapple (2006)</i></li>
+                    <p class="lead">
+                        The 33rd Waffen Grenadier Division of the SS Charlemagne (1st French) (German: 33. 
+                        Waffen-Grenadier-Division der SS "Charlemagne" (französische Nr. 1)) and 
+                        Charlemagne Regiment are collective names used for units of French 
+                        volunteers in the Wehrmacht and later Waffen-SS during World War II.
+                    </p>
+                </div>
+                <div class="col-md col-sm-8 col-md-6 col-lg-4 order-1 order-md-12 mx-auto pb-3">
+                    <a href="book_sections/bk10_grapple.html">
+                        <!--Picture of Era here!-->
+                        <img src="http://ecx.images-amazon.com/images/I/51b5U3LMbpL.jpg" 
+                        class="w-75 p-3" alt="Book cover of The Grapple">
+                    </a> 
+                </div>   
+            </div> 
+            <hr>
+
+            <!-- BOOK 11 -->
+            <div class="row">
+                <div class="col-md order-12">
+                    <li class="h6"><i>Settling Accounts: In at the Death (2007)</i></li>
+                    <p class="lead">
+                        The 33rd Waffen Grenadier Division of the SS Charlemagne (1st French) (German: 33. 
+                        Waffen-Grenadier-Division der SS "Charlemagne" (französische Nr. 1)) and 
+                        Charlemagne Regiment are collective names used for units of French 
+                        volunteers in the Wehrmacht and later Waffen-SS during World War II.
+                    </p>
+                </div>
+                <div class="col-md col-sm-8 col-md-6 col-lg-4 order-1 mx-auto pb-3">
+                    <a href="book_sections/bk11_death.html">
+                        <!--Picture of Era here!-->
+                        <img src="http://img2.imagesbn.com/p/9780345500519_p0_v1_s260x420.JPG" 
+                        class="w-75 p-3" alt="Book cover of In at the Death">
+                    </a> 
+                </div>   
+            </div> 
+
+        </ul>
+    </div>
+
+    <!--HAVE FOOTER HERE FOR BACKEND CODE WHICH WILL DISPLAY CONTACT INFO OR SUGGESTIONS ON SITE-->
+   <!-- Look at Udemy Web developer course for installing ejs and having the footer here for all files! -->
+    <hr>    
+    <footer class="bg-dark text-white">
+        <p>Long John Johnsons!</p>
+    </footer>
+
+<!-- ///// -->
+<!-- Footer -->
+<?php include('assets/footer.php') ?>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
